7f15a762c7dc86573327f79bd0ba00f72e3fa2e5
mabdelawal <mohammedabdelawaldeveloper@gmail.com>
2024-02-19 19:09:29 +0200

    define the models:  - Borrower  - BorrowRecord  - Author  - Book

--- book/models.py
+++ book/models.py
@@ -1,1 +1,49 @@
-# Create your models here.
+import datetime
+
+from django.db import models
+from django.db.models import Count
+
+
+class BookManager(models.Manager):
+    def get_queryset(self):
+        # To avoid N+1 issue
+        return super().get_queryset().annotate(borrowed_count=Count('borrow_records'))
+
+
+class Author(models.Model):
+    """Model representing an author."""
+    name: str = models.CharField(max_length=255, verbose_name="Name")
+    biography: str = models.TextField(blank=True, verbose_name="Biography")
+    birth_date: datetime.date = models.DateField(null=True, blank=True, verbose_name="Birth Date")
+    death_date: datetime.date = models.DateField(null=True, blank=True, verbose_name="Death Date")
+
+    def __str__(self):
+        """Return the name of the author."""
+        return self.name
+
+
+class Book(models.Model):
+    """Model representing a book in the library."""
+    title: str = models.CharField(max_length=255, verbose_name="Title")
+    author: Author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, verbose_name="Author")
+    isbn: str = models.CharField(max_length=13, unique=True, verbose_name="ISBN")
+    genre: str = models.CharField(max_length=100, verbose_name="Genre")
+    publication_date: datetime.date = models.DateField(verbose_name="Publication Date")
+    description: str = models.TextField(verbose_name="Description")
+    total_count: int = models.PositiveIntegerField(default=1, verbose_name="Total Count")
+
+    objects = BookManager()
+
+    @property
+    def borrowed_count(self) -> int:
+        """Calculate and return the count of borrowed books."""
+        return self.borrowed_count
+
+    @property
+    def available_count(self) -> int:
+        """Calculate and return the count of available books."""
+        return self.total_count - self.borrowed_count
+
+    def __str__(self) -> str:
+        """Return the title of the book."""
+        return self.title
--- library/models.py
+++ library/models.py
@@ -1,1 +1,31 @@
-# Create your models here.
+import datetime
+
+from django.db import models
+
+from book.models import Book
+
+
+class Borrower(models.Model):
+    """Model representing individuals borrowing books."""
+    name: str = models.CharField(max_length=100, verbose_name="Name")
+    email: str = models.EmailField(unique=True, verbose_name="Email")
+    phone_number: str = models.CharField(max_length=20, verbose_name="Phone Number")
+
+    def __str__(self) -> str:
+        """Return the name of the borrower."""
+        return self.name
+
+
+class BorrowRecord(models.Model):
+    """Model representing a record of book borrowings."""
+    book: Book = models.ForeignKey(
+        Book, on_delete=models.CASCADE,
+        verbose_name="Book", related_name="borrow_records"
+    )
+    borrower: Borrower = models.ForeignKey(Borrower, on_delete=models.CASCADE, verbose_name="Borrower")
+    borrow_date: datetime.date = models.DateField(auto_now_add=True, verbose_name="Borrow Date")
+    return_date: datetime.date = models.DateField(null=True, blank=True, verbose_name="Return Date")
+
+    def __str__(self) -> str:
+        """Return a string representation of the borrowing record."""
+        return f"{self.borrower.name} borrows {self.book.title}"
